369789d66fd02f6e57b0c8986a3e2be2f2080194
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-03-10 12:05:45 +0100

    passing tests for min and max temps added

--- jasmine-standalone-3.6.0/src/thermostat.js
+++ jasmine-standalone-3.6.0/src/thermostat.js
@@ -4,6 +4,8 @@ class Thermostat {
         this.MINIMUM_TEMPERATURE = 10;
     this.temperature = 20;
     this.powerSavingMode = true;
+    this.MAX_LIMIT_PSM_ON = 25;
+    this.MAX_LIMIT_PSM_OFF = 32;
     }
 
     getCurrentTemperature() {
@@ -11,6 +13,9 @@ class Thermostat {
     }
 
     up() {
+        if (this.isMaximumTemperature()){
+            return;
+        }
         this.temperature ++
     }
 
@@ -35,5 +40,13 @@ class Thermostat {
 
     isMinimumTemperature() {
         return this.temperature === this.MINIMUM_TEMPERATURE;
+    }
+
+    isMaximumTemperature() {
+        if (this.isPowerSavingModeOn() === false) {
+            return this.temperature === this.MAX_LIMIT_PSM_OFF;
+        }
+        return this.temperature === this.MAX_LIMIT_PSM_ON;
+        
     }
 }

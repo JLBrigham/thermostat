1a2ebdab815cb3b7a7c3db02f42306e9a563621b
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-03-11 16:45:25 +0100

    connect functionality with jquery

---
+++ jasmine-standalone-3.6.0/interface.js
@@ -0,0 +1,37 @@
+$(document).ready(function() { 
+
+var thermostat = new Thermostat();
+
+updateTemperature();
+
+$('#temperature-up').click(function() {
+    thermostat.up();
+    updateTemperature();
+});
+
+$('#temperature-down').click(function() {
+    thermostat.down();
+    updateTemperature();
+});
+
+$('#temperature-reset').click(function() {
+    thermostat.resetTemperature();
+    updateTemperature();
+});
+
+$('#powersaving-off').click(function() {
+    thermostat.switchPowerSavingModeOff();
+    $('#power-saving-status').text('off');
+});
+
+$('#powersaving-on').click(function() {
+    thermostat.switchPowerSavingModeOn();
+    $('#power-saving-status').text('on');
+});
+
+function updateTemperature() {
+    $('#temperature').text(thermostat.getCurrentTemperature());
+    $('#temperature').attr('class', thermostat.energyUsage());
+}
+
+});
